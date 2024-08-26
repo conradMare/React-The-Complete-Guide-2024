6795851e00dee0a1b548b9eaeac7da67d14017ce
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-26 07:52:44 +0200

    2024/08/26(am)
    S01.04_Editing Our First React App - Project Complete

--- Section01_GettingStarted/S01.04_EditingOurFirstReactApp/Editing-Our-First-React-App/src/App.jsx
+++ Section01_GettingStarted/S01.04_EditingOurFirstReactApp/Editing-Our-First-React-App/src/App.jsx
@@ -61,6 +61,12 @@ function App() {
           >
             Related Resources
           </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            Editing Our First React App Challenge
+          </button>
         </menu>
         <div id="tab-content">
           <ul>
