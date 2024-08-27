966c8ba2953560fae3d12a4f1b4ff430fd8d5aa5
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-27 07:37:39 +0200

    2024/08/27(am)
    S02.23_Arrays & Array Methods like map() - Complete

--- Section02_JavaScriptRefresher/S02.23_ArraysAndArrayMethodsLikeMap/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.23_ArraysAndArrayMethodsLikeMap/code/finished/assets/scripts/app.js
@@ -1,1 +1,23 @@
 // Arrays and Array Methods lik map()
+
+const hobbies = ["Golf", "Cooking", "Reading"];
+console.log(hobbies[0]);
+
+hobbies.push("Working");
+console.log(hobbies);
+
+// Find the Index of a item in an Array -> takes a function as a parameter
+const index = hobbies.findIndex((item) => item === "Reading");
+
+console.log(index);
+
+// Map - Allows you to transform every item in an array to another item (Also takes a function as input)
+// const editedHobbies = hobbies.map((item) => item + "!");
+// console.log(editedHobbies);
+
+// Map can also take objects as input
+const updatedHobbies = hobbies.map((item) => ({
+    text: item
+}));
+
+console.log(updatedHobbies);
