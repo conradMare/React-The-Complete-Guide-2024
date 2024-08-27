9d143de61b9b1ad6d25f4c817b1f85ddb7274a26
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-27 06:42:29 +0200

    2024/08/27(am)
    S02.20_Arrow Functions - Complete

--- Section02_JavaScriptRefresher/S02.20_ArrowFunctions/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.20_ArrowFunctions/code/finished/assets/scripts/app.js
@@ -1,1 +1,14 @@
 // Arrow Functions
+
+// Anonymous Function:
+// export default function () {
+//     console.log("Hello!");
+// }
+
+// Arrow Function
+export default (userName, message) => {
+    console.log("Hello");
+    return userName + message;
+};
+
+console.log(userName)
