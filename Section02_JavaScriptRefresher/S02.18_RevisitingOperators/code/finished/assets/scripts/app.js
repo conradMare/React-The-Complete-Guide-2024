b7ce8655f6bf46d90e23958a83357b022113fc0e
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-27 06:03:18 +0200

    2024/08/27(am)
    S02.18_Revisiting Operators - Complete

--- Section02_JavaScriptRefresher/S02.18_RevisitingOperators/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.18_RevisitingOperators/code/finished/assets/scripts/app.js
@@ -1,1 +1,10 @@
 // Revisiting Operators
+
+// console.log(10 + 5);
+// console.log("Hello " + "World!");
+// console.log(10 === 5);
+// console.log(10 >= 10);
+
+if (10 === 10) {
+    console.log("Hello World!");
+}
