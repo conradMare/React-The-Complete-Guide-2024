9e2bfabd600f6b316563635e97f104f1b396433d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-27 05:29:23 +0200

    2024/08/27(am)
    S02.16_"import" & "export" - Complete

---
+++ Section02_JavaScriptRefresher/S02.16_ImportAndExport/code/finished/assets/scripts/app.js
@@ -0,0 +1,17 @@
+// import { apiKey } from "./util.js";
+
+// import apiKey from "./util.js";
+// import { apiKey2, abc } from "./util.js";
+
+// import * as util from "./util.js";
+
+import { apiKey2, abc as content } from "./util.js";
+
+// console.log(apiKey);
+// console.log(apiKey2);
+// console.log(abc);
+
+// console.log(util.abc);
+
+console.log(apiKey2);
+console.log(content);
---
+++ Section02_JavaScriptRefresher/S02.16_ImportAndExport/code/finished/assets/scripts/util.js
@@ -0,0 +1,4 @@
+// export let apiKey = "assdffgfdafgad";
+export let apiKey2 = "apiKey2";
+export let abc = "abc";
+export default "apiKey";
