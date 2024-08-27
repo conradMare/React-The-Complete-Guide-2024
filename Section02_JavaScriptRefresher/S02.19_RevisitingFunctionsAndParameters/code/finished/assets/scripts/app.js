2171ed7cbea5d08dcf363b231b909c6baaee7e4d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-27 06:22:18 +0200

    2024/08/27(am)
    S02.19_Revisiting Functions and Params - Complete

--- Section02_JavaScriptRefresher/S02.19_RevisitingFunctionsAndParameters/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.19_RevisitingFunctionsAndParameters/code/finished/assets/scripts/app.js
@@ -1,1 +1,13 @@
 // Revisiting Functions & Parameters
+
+function createGreeting(userName, message = "Hello!") {
+    // console.log(userName);
+    // console.log(message);
+    return "Hi, I am " + userName + ", " + message;
+}
+
+const greeting1 = createGreeting("Conrad");
+console.log(greeting1);
+
+const greeting2 = createGreeting("Max", "What's up?");
+console.log(greeting2);
