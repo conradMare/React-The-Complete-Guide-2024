20ac951cc7074461c0b1d4de6bce6789f0cd987f
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-28 06:52:04 +0200

    2024/08/28(am)
    S02.29_Using Functions as Values - Complete

--- Section02_JavaScriptRefresher/S02.29_UsingFunctionsAsValues/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.29_UsingFunctionsAsValues/code/finished/assets/scripts/app.js
@@ -1,3 +1,23 @@
 // Using Functions as Values
 
-alert('Hello React!');
+function handleTimeout() {
+    console.log("Timed Out!");
+}
+
+function handleTimeout2() {
+    console.log("Timed Out... Again");
+}
+
+setTimeout(handleTimeout, 1000);
+setTimeout(handleTimeout2, 2000);
+setTimeout(() => {
+    console.log("More timing out");
+}, 3000);
+
+function greeter(greetFn) {
+    greetFn();
+}
+
+greeter(() => {
+    console.log("Hi");
+})
