a3923d119da257b671b688bd6cba84f8e0d6b3dc
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-28 06:16:59 +0200

    2024/08/28(am)
    S02.27_Revisiting Control Structures - Complete

--- Section02_JavaScriptRefresher/S02.27_RevisitingControlStructures/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.27_RevisitingControlStructures/code/finished/assets/scripts/app.js
@@ -1,2 +1,18 @@
 // Revisiting Control Structures
-alert('Hello React');
+
+const password = prompt("Your Password");
+
+if (password === "Hello") {
+    console.log("Hello works");
+} else if (password === "hello") {
+    console.log("hello with a lower case works");
+} else {
+    console.log("Access not granted");
+}
+
+// for-of loop
+const hobbies = ["Sport", "Cooking"];
+
+for (const hobby of hobbies) {
+    console.log(hobby);
+}
