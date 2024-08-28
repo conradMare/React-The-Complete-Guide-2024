726ff276571a201bf7ba3627da40c0e8b098371b
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-28 06:35:44 +0200

    2024/08/28(am)
    S02.28_Manipulating the DOM (Not with React!) - Complete

--- Section02_JavaScriptRefresher/S02.28_ManipulatingTheDOM-NotWithReact/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.28_ManipulatingTheDOM-NotWithReact/code/finished/assets/scripts/app.js
@@ -1,3 +1,5 @@
 // Manipulating the DOM (Not with React!)
+// WON"T BE DOING THIS IN THIS COURSE, REACT IS DECLARATIVE (MANIPULATES THE DOM FOR ME)
 
-alert("Hello React!");
+const list = document.querySelector("ul");
+list.remove();
