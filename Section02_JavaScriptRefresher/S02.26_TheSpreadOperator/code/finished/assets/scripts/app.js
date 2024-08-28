1a74d8a4dfb0598e457c893b061e807fefd5ce35
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-28 05:52:34 +0200

    2024/08/28(am)
    S02.26_The Spread Operator - Complete

--- Section02_JavaScriptRefresher/S02.26_TheSpreadOperator/code/finished/assets/scripts/app.js
+++ Section02_JavaScriptRefresher/S02.26_TheSpreadOperator/code/finished/assets/scripts/app.js
@@ -1,2 +1,23 @@
 // The Spread Operator
-alert('Hello World!');
+
+const hobbies = ["Sport", "Cooking"];
+const user = {
+    name: "John",
+    age: 35
+};
+
+const newHobbies = ["Reading"];
+
+const mergedList = [...hobbies, ...newHobbies];
+console.log(mergedList);
+
+const extendedUser = {
+    isAdmin: true,
+    ...user
+};
+console.log(extendedUser);
+
+// The Spread Operator [...<arrayName>] "pulls out" values from an array
+// and adds them to another array as COMMA SEPARATED VALUES,
+// if you don't use the spread operator it will created an array
+// with another NESTED ARRAY.
