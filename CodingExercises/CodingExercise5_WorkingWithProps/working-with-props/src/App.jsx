18e1f2284d4e4a79596be36d0a71a620b2e0ba37
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-29 08:01:22 +0200

    2024/08/29(am)
    Coding Exercise 5: Working with Props - Complete

--- CodingExercises/CodingExercise5_WorkingWithProps/working-with-props/src/App.jsx
+++ CodingExercises/CodingExercise5_WorkingWithProps/working-with-props/src/App.jsx
@@ -1,8 +1,8 @@
-export function CourseGoal() {
+export function CourseGoal(props) {
   return (
     <li>
-      <h2>TITLE</h2>
-      <p>DESCRIPTION</p>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
     </li>
   );
 }
@@ -15,6 +15,18 @@ function App() {
       <ul>
         {/* OUTPUT AT LEAST TWO CourseGoal components here */}
         {/* One of them should have a title of “Learn React” and a description of “In-depth” */}
+        <CourseGoal
+          title="Learn React"
+          description="In-depth"
+        />
+        <CourseGoal
+          title="Learn Next.js"
+          description="Get the what's what"
+        />
+        <CourseGoal
+          title="Learn Tailwind"
+          description="Keep up to date with new tech"
+        />
       </ul>
     </div>
   );
