141c7fa0b8755abdf9db9257e37a99ef20e86eee
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-31 05:37:57 +0200

    2024/08/51(am)
    S03.50_Reacting to Events [Core Concept] - Complete

--- ReactFundamentalsProject/react-project-1/src/components/TabButton.jsx
+++ ReactFundamentalsProject/react-project-1/src/components/TabButton.jsx
@@ -1,6 +1,13 @@
 function TabButton({ children }) {
+
+    function handleClick() {
+        console.log("Hello World!");
+    }
+
     return (
-        <li><button>{children}</button></li>
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
     );
 }
 
