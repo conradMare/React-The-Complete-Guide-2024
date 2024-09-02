135254007442d2e048754bc28bc513cb73399c93
conradMare <158014474+conradMare@users.noreply.github.com>
2024-09-02 07:07:36 +0200

    2024/09/02(am)
    Coding Exercise 10: Conditional Content - Project Description Added

--- CodingExercises/CodingExercise10_ConditionalContent/conditional-content/src/App.jsx
+++ CodingExercises/CodingExercise10_ConditionalContent/conditional-content/src/App.jsx
@@ -1,3 +1,7 @@
+// You're working on a part of a web app that's responsible for showing a warning when a user is about to perform a dangerous action.
+// Therefore, your task is to conditionally show a warning box once a user has clicked a specific button.
+// Inside that warning dialog, another button allows users to dismiss the warning (i.e., remove the warning box from the screen).
+
 import React from 'react';
 
 // IMPORTANT:
