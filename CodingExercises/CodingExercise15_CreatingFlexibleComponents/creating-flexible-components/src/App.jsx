e373e965a7496b0189d035040009a074553e9b6d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-09-04 07:03:32 +0200

    2024/09/04(am)
    Coding Exercise 15: Creating Flexible Components - Project Setup

---
+++ CodingExercises/CodingExercise15_CreatingFlexibleComponents/creating-flexible-components/src/App.jsx
@@ -0,0 +1,94 @@
+// Your task is to build a highly re-usable,
+// custom Button component that can be used in all the following ways (also see the code in the App.js file):
+
+// "Filled" mode (default):
+
+{/* <Button>Default</Button>
+or
+
+<Button mode="filled">Filled</Button> */}
+
+// "Outline" mode:
+
+{/* <Button mode="outline">Outline</Button> */ }
+
+// "Text-only" mode:
+
+{/* <Button mode="text">Text</Button> */ }
+
+// With Icon:
+
+{/* <Button Icon={HomeIcon}>Home</Button>
+or
+
+<Button Icon={PlusIcon} mode="text">
+  Add
+</Button> */}
+
+// Hint: To make sure the icon becomes visible (if passed correctly to the component & used in there),
+// wrap the icon component in the button with a <span> that has the class "button-icon" on it.
+// Also wrap the children prop with a <span>!
+// You find all the styles (CSS classes) that are required to build a button that supports these different
+// "modes" in the provided index.css file!
+// All buttons need a button CSS class - and then, depending on their mode, additional classes.
+// In addition, the custom Button component must accept all standard props that could be set on the built-in <button>.
+// These props should be forwarded to the default <button> element that will be used in the custom Button component.
+// Your task therefore is to work on the Button component provided in the Button.js file.
+// Don't add multiple custom components, instead work on that one provided component and make sure that it 
+// supports all these different modes & features. Also make sure, that if no mode is set, the "filled" mode is assumed as a default.
+
+import Button from "./components/Button";
+import HomeIcon from "./components/HomeIcon";
+import PlusIcon from "./components/PlusIcon";
+
+function App() {
+  return (
+    <div id="app">
+      <section>
+        <h2>Filled Button (Default)</h2>
+        <p>
+          <Button>Default</Button>
+        </p>
+        <p>
+          <Button mode="filled">Filled (Default)</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Outline</h2>
+        <p>
+          <Button mode="outline">Outline</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Text-only Button</h2>
+        <p>
+          <Button mode="text">Text</Button>
+        </p>
+      </section>
+      <section>
+        <h2>Button with Icon</h2>
+        <p>
+          <Button Icon={HomeIcon}>Home</Button>
+        </p>
+        <p>
+          <Button Icon={PlusIcon} mode="text">
+            Add
+          </Button>
+        </p>
+      </section>
+      <section>
+        <h2>Buttons Should Support Any Props</h2>
+        <p>
+          <Button mode="filled" disabled>
+            Disabled
+          </Button>
+        </p>
+        <p>
+          <Button onClick={() => console.log('Clicked!')}>Click me</Button>
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
