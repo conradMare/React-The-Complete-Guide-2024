45953a5e009429e5458425a7514fac9788e61161
conradMare <158014474+conradMare@users.noreply.github.com>
2024-09-05 07:20:41 +0200

    2024/09/05(am)
    S04.75_Concept Repetition: Working with State - Complete

--- ReactTicTacToeProject/tic-tac-toe/src/components/Player.jsx
+++ ReactTicTacToeProject/tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+
+    let userEdit = <span className="player-name">{name}</span>;
+
+    if (isEditing) {
+        userEdit = <input type="text" required />;
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {userEdit}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
