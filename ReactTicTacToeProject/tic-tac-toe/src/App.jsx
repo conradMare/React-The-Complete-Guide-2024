af8694a3788f96d163880bf0b92122bc7e69c982
conradMare <158014474+conradMare@users.noreply.github.com>
2024-09-09 07:11:59 +0200

    2024/09/09(am)
    S04.95_An Alternative To Lifting State Up - Complete

--- ReactTicTacToeProject/tic-tac-toe/src/App.jsx
+++ ReactTicTacToeProject/tic-tac-toe/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -92,11 +92,13 @@ function App() {
             initialName="Player 1"
             symbol="X"
             isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
           />
           <Player
             initialName="Player 2"
             symbol="O"
             isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
           />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
