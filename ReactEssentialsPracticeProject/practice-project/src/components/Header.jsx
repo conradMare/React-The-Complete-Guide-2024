62dab496c046e2a95f82679d722337b6bcc8a36c
conradMare <158014474+conradMare@users.noreply.github.com>
2024-09-11 07:19:05 +0200

    2024/09/11(am)
    S05.98_Adding a Header Component - Complete

---
+++ ReactEssentialsPracticeProject/practice-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import investmentCalculatorLogo from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+    return (
+        <header id="header">
+            <img src={investmentCalculatorLogo} alt="Logo showing a money bag" />
+            <h1>React Investment Calculator</h1>
+        </header>
+    );
+}
