46ead76d43093f087fdb9197907b4c0f8ae0aeab
conradMare <158014474+conradMare@users.noreply.github.com>
2024-09-17 05:28:41 +0200

    2024/09/17(am)
    Coding Exercise 19: Dynamic Styles - Project Setup

---
+++ CodingExercises/CodingExercise19_DynamicStyles/dynanmic-styles/src/App.jsx
@@ -0,0 +1,18 @@
+// Your task is to dynamically apply a style (color: red) to the <p>Style me</p> element in the provided React app.
+
+// The style should be applied as an inline style (i.e., via the style attribute / prop) when the <button> is clicked for the first time.
+// Once the button is clicked again, the styling should switch back to color: white, which should also be the initial style.
+
+// Make sure that the button toggles between these two styles (color: white <=> color: red).
+
+import React from 'react';
+
+// don't change the Component name "App"
+export default function App() {
+  return (
+    <div>
+      <p>Style me!</p>
+      <button>Toggle style</button>
+    </div>
+  );
+}
