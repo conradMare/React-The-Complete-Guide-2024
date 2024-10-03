f207e204ae9cc9388507e9067f2d172f7ce6a4c2
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-03 09:27:08 +0200

    2024/10/03(am)
    Coding Exercise 27: Using useReducer() -  Complete

--- CodingExercises/CodingExercise27_UsingUseReducer/using-usereducer/src/App.jsx
+++ CodingExercises/CodingExercise27_UsingUseReducer/using-usereducer/src/App.jsx
@@ -18,18 +18,41 @@
 // (it must be named type - other names will make the automatic checks fail!) 
 // that contains either 'INCREMENT', 'DECREMENT' or 'RESET' as a value!
 
-export function counterReducer() { }
+import { useReducer } from 'react';
+
+export function counterReducer(state, action) {
+  if (action.type === 'INCREMENT') {
+    return { count: state.count + 1 };
+  } else if (action.type === 'DECREMENT') {
+    return { count: state.count - 1 };
+  } else if (action.type === 'RESET') {
+    return { count: 0 };
+  }
+
+  return state;
+}
 
 function App() {
+  const [counterState, dispatchCounterAction] = useReducer(
+    counterReducer,
+    { count: 0 }
+  );
+
   return (
     <div id="app">
       <h1>The (Final?) Counter</h1>
       <p id="actions">
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <button onClick={() => dispatchCounterAction({ type: 'INCREMENT' })}>
+          Increment
+        </button>
+        <button onClick={() => dispatchCounterAction({ type: 'DECREMENT' })}>
+          Decrement
+        </button>
+        <button onClick={() => dispatchCounterAction({ type: 'RESET' })}>
+          Reset
+        </button>
       </p>
-      <p id="counter"></p>
+      <p id="counter">{counterState.count}</p>
     </div>
   );
 }
