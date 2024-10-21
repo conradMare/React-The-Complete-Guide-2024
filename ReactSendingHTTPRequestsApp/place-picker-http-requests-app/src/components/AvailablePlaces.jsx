3d0ce3c02e8e8794ab60524f18c1bd1eb8a32eb0
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-21 09:25:03 +0200

    2024/10/21(am)
    S15.237_Transforming Fetched Data - Complete

--- ReactSendingHTTPRequestsApp/place-picker-http-requests-app/src/components/AvailablePlaces.jsx
+++ ReactSendingHTTPRequestsApp/place-picker-http-requests-app/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 import Places from './Places.jsx';
 import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   // Todo: Fetch available places from backend API
@@ -14,18 +15,26 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/placessss');
+        const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
 
         if (!response.ok) {
           throw new Error('Failed to fetch places data');
         }
-        setAvailablePlaces(resData.places);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({ message: error.message || 'Could not fetch message, please try again later' });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
