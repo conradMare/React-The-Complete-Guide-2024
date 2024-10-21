5ed6f7e31989de07436240fccfd357a70f30adf8
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-21 07:11:18 +0200

    2024/10/21(am)
    S15.230_Preapring the App For Data Fetching - Complete

--- ReactSendingHTTPRequestsApp/place-picker-http-requests-app/src/components/AvailablePlaces.jsx
+++ ReactSendingHTTPRequestsApp/place-picker-http-requests-app/src/components/AvailablePlaces.jsx
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 import Places from './Places.jsx';
 
+const places = localStorage.getItem();
+
 export default function AvailablePlaces({ onSelectPlace }) {
+  // Todo: Fetch available places from backend API
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
