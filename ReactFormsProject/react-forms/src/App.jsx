ce07729a4bacfa93c764dc7099aa16494c7c6855
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-29 05:55:58 +0200

    2024/10/29(am)
    S17.255_Getting Values via FormData & Native Browser APIs - Complete

--- ReactFormsProject/react-forms/src/App.jsx
+++ ReactFormsProject/react-forms/src/App.jsx
@@ -1,15 +1,32 @@
+// import Header from './components/Header.jsx';
+// import Login from './components/Login.jsx';
+
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <main>
+//         <Login />
+//       </main>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
 }
 
 export default App;
+
