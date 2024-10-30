88550ad0a304235b19fc265b5d113119f7470aa8
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-30 06:18:20 +0200

    2024/10/30(am)
    S17.257_Validating Input on Every Keystroke via State - Complete

--- ReactFormsProject/react-forms/src/App.jsx
+++ ReactFormsProject/react-forms/src/App.jsx
@@ -1,32 +1,32 @@
-// import Header from './components/Header.jsx';
-// import Login from './components/Login.jsx';
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <main>
-//         <Login />
-//       </main>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import Header from './components/Header.jsx';
-import Signup from './components/Signup.jsx';
+import Login from './components/Login.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Signup />
+        <Login />
       </main>
     </>
   );
 }
 
 export default App;
+
+// import Header from './components/Header.jsx';
+// import Signup from './components/Signup.jsx';
+
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <main>
+//         <Signup />
+//       </main>
+//     </>
+//   );
+// }
+
+// export default App;
 
