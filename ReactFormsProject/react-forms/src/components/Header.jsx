0751bd6db6350285777a3b40dbc18522b5b2e38d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-30 08:42:46 +0200

    2024/10/30(am)
    React Forms Project - Updated

--- ReactFormsProject/react-forms/src/components/Header.jsx
+++ ReactFormsProject/react-forms/src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
     <header>
       <img src={logoImg} alt="A form and a pencil" />
       <h1>React Forms</h1>
+      <h2 style={{ color: "black" }}>"A sure sign of wisdom is constant cheerfulness." - Michel de Montaigne</h2>
     </header>
   );
 }
