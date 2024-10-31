13bcdae14c65770a508359aaa90d68f7ce8ee46d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-10-31 06:11:00 +0200

    2024/10/31(am)
    React Forms Project - Updated

--- ReactFormsProject/react-forms/src/App.jsx
+++ ReactFormsProject/react-forms/src/App.jsx
@@ -30,22 +30,6 @@
 
 // export default App;
 
-// import Header from './components/Header.jsx';
-// import Signup from './components/Signup.jsx';
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <main>
-//         <Signup />
-//       </main>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import Header from './components/Header.jsx';
 import Login from './components/StateLogin.jsx';
 
