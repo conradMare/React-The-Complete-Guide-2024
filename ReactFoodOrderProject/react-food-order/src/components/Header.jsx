5c9c2f090bf77333d39b38f6d22efb337315de3b
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-04 10:10:03 +0200

    2024/11/04(am)
    S18.271_Creating a Configurable & Flexible Custom Button Component - Complete

--- ReactFoodOrderProject/react-food-order/src/components/Header.jsx
+++ ReactFoodOrderProject/react-food-order/src/components/Header.jsx
@@ -1,15 +1,16 @@
+import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg';
 
 export default function Header() {
     return (
         <header id="main-header">
             <div id="title">
-                <img src={logoImg} alt="A food order application" />
-                <h1>Food for Thought</h1>
+                <img src={logoImg} alt="A restaurant" />
+                <h1>ReactFood</h1>
             </div>
             <nav>
-                <button>Cart (0)</button>
+                <Button textOnly>Cart (0)</Button>
             </nav>
         </header>
-    )
+    );
 }
