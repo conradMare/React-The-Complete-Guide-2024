2e1670e1fcc69ad49491c1acc677d0f4cea1aee6
conradMare <158014474+conradMare@users.noreply.github.com>
2024-11-07 13:30:39 +0200

    2024/11/07(am)
    Food Order App - Footer added

--- ReactFoodOrderProject/react-food-order/src/App.jsx
+++ ReactFoodOrderProject/react-food-order/src/App.jsx
@@ -2,6 +2,7 @@ import Cart from './components/Cart.jsx';
 import Checkout from './components/Checkout.jsx';
 import Header from './components/Header.jsx';
 import Meals from './components/Meals.jsx';
+import Footer from './components/Footer.jsx';
 import { CartContextProvider } from './store/CartContext.jsx';
 import { UserProgressContextProvider } from './store/UserProgressContext.jsx';
 
@@ -11,6 +12,7 @@ function App() {
       <CartContextProvider>
         <Header />
         <Meals />
+        <Footer />
         <Cart />
         <Checkout />
       </CartContextProvider>
