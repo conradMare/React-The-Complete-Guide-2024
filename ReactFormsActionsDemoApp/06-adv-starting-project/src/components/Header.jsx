468bf90dcf0462737a8bb4de9640980db237a196
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-02 09:19:59 +0200

    2025/01/02(am)
    React Forms with Actions: Demo Project - Main Header Updated

--- ReactFormsActionsDemoApp/06-adv-starting-project/src/components/Header.jsx
+++ ReactFormsActionsDemoApp/06-adv-starting-project/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header id="main-header">
-      <h1>OpinionBoard</h1>
+      <h1>"Opinion" Board</h1>
       <p>
         Strong opinions, judged by anonymous internet users. What could possibly
         go wrong?
