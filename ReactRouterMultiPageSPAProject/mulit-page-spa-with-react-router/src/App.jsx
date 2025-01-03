a36e205495297d2d2e5613e55e2e038ea02e43a9
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-03 09:13:32 +0200

    2025/01/03(am)
    React Router Multi-Page SPA Application - Changes Reverted

--- ReactRouterMultiPageSPAProject/mulit-page-spa-with-react-router/src/App.jsx
+++ ReactRouterMultiPageSPAProject/mulit-page-spa-with-react-router/src/App.jsx
@@ -1,9 +1,6 @@
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Module Introduction - Complete</h1>
-      <p>Building a Multi-Page SPA with React Router</p>
-    </div>
+    <div></div>
   );
 }
 
