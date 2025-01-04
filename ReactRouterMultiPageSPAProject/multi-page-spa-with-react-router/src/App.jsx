cfc8c5eca1a18f5c4362984e9fb64f954ede1d75
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-04 08:25:46 +0200

    2025/01/04(am)
    React Router Multi-Page SPA Project - Renamed, Styling Update, Home Page Update

---
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: 'black', textAlign: 'center' }}>
+        Hello World!
+      </h1>
+      <hr style={{ width: '50%' }} />
+      <p style={{ color: 'black', textAlign: 'center' }}>
+        This is a test!
+      </p>
+    </div>
+  );
+}
+
+export default App;
