7729b824b8794dbe6d41658496085ab94fee804a
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-06 09:33:59 +0200

    2025/01/06(am)
    S23.345_Exploring an Alternative Way of Defining Routes - Complete

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
@@ -1,7 +1,17 @@
+// import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// )
+
+// const routerValue = createBrowserRouter(routeDefinitions);
 
 const routerValue = createBrowserRouter([
   { path: '/', element: <HomePage /> },
