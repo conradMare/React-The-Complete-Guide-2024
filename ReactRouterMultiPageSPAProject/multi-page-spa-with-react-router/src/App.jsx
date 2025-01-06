8075681f30b154ed84c28faa9266aef8c3c0095b
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-06 09:12:33 +0200

    2025/01/06(am)
    S23.343_Defining Routes - Imports Update

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 
