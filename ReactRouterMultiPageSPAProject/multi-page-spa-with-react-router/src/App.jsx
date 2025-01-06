648a6142d5680512b27ab381c2ee68bd70fbba59
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-06 08:22:33 +0200

    2025/01/06(am)
    S23.341_Routing: Multiple Pages in a Single-Page Application - Complete

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
@@ -1,14 +1,6 @@
 function App() {
   return (
-    <div>
-      <h1 style={{ color: 'black', textAlign: 'center' }}>
-        Hello World!
-      </h1>
-      <hr style={{ width: '50%' }} />
-      <p style={{ color: 'black', textAlign: 'center' }}>
-        This is a test!
-      </p>
-    </div>
+    <div></div>
   );
 }
 
