06973f08fed4c65fe1737a4b3aa4e86025bc5694
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-07 08:16:03 +0200

    2025/01/07(am)
    S23.346_Navigating between pages with Links - Complete

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/pages/Home.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function HomePage() {
     return (
-        <h1>This is the Home Page!</h1>
+        <>
+            <h1>This is the Home Page!</h1>
+            <p>Go to <Link to="/products">the list of products</Link>.</p>
+        </>
     );
 }
 
