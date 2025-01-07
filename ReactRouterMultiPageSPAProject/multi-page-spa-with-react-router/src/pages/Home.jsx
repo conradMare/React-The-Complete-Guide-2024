ed1dc750dfcedeb39ed14b2d93f2bb67c0ff7d5a
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-07 09:15:48 +0200

    2025/01/07(am)
    React Router Multi-Page SPA Project - Styling Update

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/pages/Home.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import classes from './Home.module.css';
+
 function HomePage() {
     return (
         <>
             <h1>This is the Home Page!</h1>
-            <p>Go to <Link to="/products">the list of products</Link>.</p>
+            <p className={classes.paragraph}>Go to <Link to="/products">the list of products</Link>.</p>
         </>
     );
 }
