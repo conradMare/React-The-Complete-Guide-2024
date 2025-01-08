c2263404fd36c69ba96a21c322b2bc95995b5b83
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-08 09:31:07 +0200

    2025/01/08(am)
    S23.351_Defining & Using Dynamic Routes - Complete

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/ErrorPage';
+import ProductDetailPage from './pages/ProductDetail';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -22,7 +23,8 @@ const routerValue = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> },
-      { path: '/products', element: <ProductsPage /> }
+      { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage /> }
     ],
   },
 ])
