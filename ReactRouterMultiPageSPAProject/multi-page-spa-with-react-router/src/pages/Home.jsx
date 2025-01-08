8b7c90b31a0f963ebe9bfe9a2e622743384e143c
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-08 08:52:04 +0200

    2025/01/08(am)
    S23.350_Navigating Programmatically - Complete

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/pages/Home.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/pages/Home.jsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import classes from './Home.module.css';
 
 function HomePage() {
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate('/products');
+    }
+
     return (
         <>
             <h1>This is the Home Page!</h1>
-            <p className={classes.paragraph}>Go to <Link to="/products">the list of products</Link>.</p>
+            <p className={classes.paragraph}>
+                Go to <Link to="/products">the list of products</Link>.
+            </p>
+            <button onClick={navigateHandler}>Navigate</button>
         </>
     );
 }
