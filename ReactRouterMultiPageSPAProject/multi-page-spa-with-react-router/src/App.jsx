fcb926d7b4d85447cae2fbef18f4b45ef7d13d48
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-10 08:07:24 +0200

    2025/01/10(am)
    React Router Multi-Page SPA Project - Update

--- ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
+++ ReactRouterMultiPageSPAProject/multi-page-spa-with-react-router/src/App.jsx
@@ -1,4 +1,3 @@
-// import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
@@ -6,15 +5,6 @@ import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/ErrorPage';
 import ProductDetailPage from './pages/ProductDetail';
-
-// const routeDefinitions = createRoutesFromElements(
-//   <Route>
-//     <Route path='/' element={<HomePage />} />
-//     <Route path='/products' element={<ProductsPage />} />
-//   </Route>
-// )
-
-// const routerValue = createBrowserRouter(routeDefinitions);
 
 const routerValue = createBrowserRouter([
   {
