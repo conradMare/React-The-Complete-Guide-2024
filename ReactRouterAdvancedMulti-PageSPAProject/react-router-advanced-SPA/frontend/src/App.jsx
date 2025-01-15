f9c1b807a8006b0efe9ec6e8613d4382ea3938e4
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-15 08:34:10 +0200

    2025/01/15(am)
    React Router Advanced Multi-Page SPA Project - Bonus (Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components) - Complete

--- ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/App.jsx
+++ ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/App.jsx
@@ -8,6 +8,7 @@ import EventsPage from './pages/EventsPage';
 import HomePage from './pages/HomePage';
 import NewEventPage from './pages/NewEventPage';
 import RootLayout from './pages/Root';
+import EventsRoots from './pages/EventsRoots';
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
 //    - HomePage
@@ -35,10 +36,14 @@ const routeValues = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'events', element: <EventsPage /> },
-      { path: 'events/:eventId', element: <EventDetailPage /> },
-      { path: 'events/new', element: <NewEventPage /> },
-      { path: 'events/:eventId/edit', element: <EditEventPage /> },
+      {
+        path: 'events', element: <EventsRoots />, children: [
+          { index: true, element: <EventsPage /> },
+          { path: ':eventId', element: <EventDetailPage /> },
+          { path: 'new', element: <NewEventPage /> },
+          { path: ':eventId/edit', element: <EditEventPage /> },
+        ]
+      },
     ],
   },
 ])
