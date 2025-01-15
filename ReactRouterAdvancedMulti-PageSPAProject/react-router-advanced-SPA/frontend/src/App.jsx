41c0928ce9583895c1bca9e9d3d906d487c32788
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-15 08:00:52 +0200

    2025/01/15(am)
    React Router Advanced Multi-Page SPA Project - Change paths from Absolute to Relative

--- ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/App.jsx
+++ ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/App.jsx
@@ -35,10 +35,10 @@ const routeValues = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: '/events', element: <EventsPage /> },
-      { path: '/events/:eventId', element: <EventDetailPage /> },
-      { path: '/events/new', element: <NewEventPage /> },
-      { path: '/events/:eventId/edit', element: <EditEventPage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/:eventId', element: <EventDetailPage /> },
+      { path: 'events/new', element: <NewEventPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> },
     ],
   },
 ])
