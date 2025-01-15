9231cbb4dc8ad4d73780d9ea389cc93603c1613d
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-15 07:47:45 +0200

    2025/01/15(am)
    React Router Advanced Multi-Page SPA Project - Step 3 (Add a Root Layout) - Complete

--- ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/App.jsx
+++ ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 // Challenge / Exercise
 
-import { createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import EditEventPage from './pages/EditEventPage';
 import EventDetailPage from './pages/EventDetailPage';
 import EventsPage from './pages/EventsPage';
 import HomePage from './pages/HomePage';
 import NewEventPage from './pages/NewEventPage';
+import RootLayout from './pages/Root';
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
 //    - HomePage
@@ -28,20 +29,24 @@ import NewEventPage from './pages/NewEventPage';
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/events' element={<EventsPage />} />
-    <Route path='/events/:eventId' element={<EventDetailPage />} />
-    <Route path='/events/new' element={<NewEventPage />} />
-    <Route path='/events/:eventId/edit' element={<EditEventPage />} />
-  </Route>
-)
+const routeValues = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: '/events', element: <EventsPage /> },
+      { path: '/events/:eventId', element: <EventDetailPage /> },
+      { path: '/events/new', element: <NewEventPage /> },
+      { path: '/events/:eventId/edit', element: <EditEventPage /> },
+    ],
+  },
+])
 
 function App() {
   return (
     <div>
-      <RouterProvider router={routeDefinitions} />
+      <RouterProvider router={routeValues} />
     </div>
   );
 }
