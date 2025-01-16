3307ab8b7b41f232df13814e9831a8d19122b712
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-16 09:33:29 +0200

    2025/01/16(am)
    React Router Advanced Multi-page SPA Project - useParams Test

--- ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/pages/EventsPage.jsx
+++ ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/pages/EventsPage.jsx
@@ -11,6 +11,17 @@ const DUMMY_EVENTS = [
     }
 ];
 
+const DUMMY_EVENTS_PARAMS_TEST = [
+    {
+        id: 't1',
+        title: 'This is the first test',
+    },
+    {
+        id: 't2',
+        title: 'This is the second test',
+    }
+];
+
 function EventsPage() {
     return (
         <>
@@ -19,6 +30,15 @@ function EventsPage() {
                 {DUMMY_EVENTS.map((event) =>
                     <li key={event.id}>
                         <Link to={event.id}>{event.title}</Link>
+                    </li>
+                )}
+            </ul>
+            <hr />
+            <h1>Params Test</h1>
+            <ul>
+                {DUMMY_EVENTS_PARAMS_TEST.map((params) =>
+                    <li key={params.id}>
+                        <Link to={params.id}>{params.title}</Link>
                     </li>
                 )}
             </ul>
