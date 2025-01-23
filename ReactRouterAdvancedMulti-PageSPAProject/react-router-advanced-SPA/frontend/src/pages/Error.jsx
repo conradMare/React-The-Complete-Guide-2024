a38522d4b2645f78f0a944cb7451070f104e4edb
conradMare <158014474+conradMare@users.noreply.github.com>
2025-01-23 08:53:29 +0200

    2025/01/23(am)
    S22.367_Extracting Error Data & Throwing Responses - Complete

--- ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/pages/Error.jsx
+++ ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/pages/Error.jsx
@@ -1,5 +1,31 @@
-export default function ErrorPage() {
+import { useRouteError } from 'react-router-dom';
+
+import PageContent from '../components/PageContent';
+import MainNavigation from '../components/MainNavigation';
+
+function ErrorPage() {
+    const error = useRouteError();
+
+    let title = 'An error occurred';
+    let message = 'Something went wrong';
+
+    if (error.status === 500) {
+        message = JSON.parse(error.data).message;
+    }
+
+    if (error.status) {
+        title = 'Not found!';
+        message = 'Could not find resource or page.';
+    }
+
     return (
-        <p>An error occurred!</p>
+        <>
+            <MainNavigation />
+            <PageContent title={title}>
+                <p>{message}</p>
+            </PageContent>
+        </>
     );
 }
+
+export default ErrorPage;
--- ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/util/EventsLoader.jsx
+++ ReactRouterAdvancedMulti-PageSPAProject/react-router-advanced-SPA/frontend/src/util/EventsLoader.jsx
@@ -4,7 +4,9 @@ export async function loader() {
     if (!response.ok) {
         // Example of displaying an error:
         // return { isError: true, message: 'Could not fetch events.' };
-        throw { message: 'Could not fetch events.' }
+        throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+            status: 500
+        });
     } else {
         return response;
     }
